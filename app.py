730d1ee1f34f44931b0c09bb89e999ba0dc7ee25
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-06-30 17:26:31 -0500

    added jsonify

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, jsonify
 
 DEBUG=True 
 PORT=8000
@@ -9,6 +9,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	return 'Hello World'
+
+@app.route('/test')
+def get_list():
+	return jsonify(['hello', 'hi', 'doink'])
 
 if __name__ == '__main__':
 	app.run(debug=DEBUG, port=PORT)
